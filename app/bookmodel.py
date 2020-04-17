a7d0574dcb5a4c02e95f5500773a3cc38b4f3ba9
Peidong Wang <wangpeidong@hotmail.com>
2020-04-17 17:37:44 +0900

    added datababase

---
+++ app/bookmodel.py
@@ -0,0 +1,29 @@
+print(f"__file__={__file__:<35} | __name__={__name__:<20} | __package__={str(__package__):<20}")
+from .main import db
+
+class BookModel(db.Model):
+    __tablename__ = "books"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+    
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+        
+    def __repr__(self):
+        return f"<Id {self.id} - Book {self.name}>"
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "author": self.author,
+            "published": self.published
+        }
+    
+db.create_all()
+db.session.commit()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,76 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost:5432/bookstore" 
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://iwwbqrkihpmcuv:bc3d652fa7db25b52de75659f3c321082e35bd99394a8e18d53ff3c54fe524f7@ec2-18-235-97-230.compute-1.amazonaws.com:5432/dbofd7nvd6f68d"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home_view():
     return "<h1>Welcome to PD-First-App</h1>"
+    
+@app.route("/name/<name>")
+def get_book_name(name):
+    return f"<h1>Name: {name}</h1>"
+    
+@app.route("/details")
+def get_book_details(): 
+    author = request.args.get("author")
+    published = request.args.get("published")
+    return f"<h1>Author: {author}, Published: {published}</h1>"
+    
+from app.bookmodel import BookModel
+
+@app.route("/add")
+def add_book():
+    name = request.args.get("name")
+    author = request.args.get("author")
+    published = request.args.get("published")
+    try:
+        book = BookModel(
+            name = name,
+            author = author,
+            published = published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return f"<h1>Book {name} added, Id {book.id}</h1>"
+    except Exception as e:
+        return str(e)
+        
+@app.route("/getall")
+def get_all():
+    try:
+        books = BookModel.query.all()
+        return jsonify([e.serialize() for e in books])
+    except Exception as e:
+        return str(e)
+        
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book = BookModel.query.filter_by(id = id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+        return str(e)
+        
+@app.route("/add/form", methods = ["GET", "POST"])
+def add_book_form():
+    if request.method == "POST":
+        name = request.form.get("name")
+        author = request.form.get("author")
+        published = request.form.get("published")
+        try:
+            book = BookModel(
+                name = name,
+                author = author,
+                published = published   
+            )
+            db.session.add(book)
+            db.session.commit()
+            return f"<h1>Book {name} added, Id {book.id}</h1>"
+        except Exception as e:
+            return str(e)
+    return render_template("getinput.html")
